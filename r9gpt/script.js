90e8a39cd8525e106a6607318d2b7674f91f8f52
R9SME <102382866+Arnavjain1009@users.noreply.github.com>
2023-04-22 22:43:29 +0100

    Add files via upload

--- r9gpt/script.js
+++ r9gpt/script.js
@@ -44,12 +44,59 @@ alert(cc);
     });
   }
   var mydata = "";
-  async function main() {
-    const data = await getDataFromFirebase();
+  async function main(e) {
+    const data1 = await getDataFromFirebase();
     
-    mydata = data;
+    mydata = data1;
     
-    console.log(data);
+    console.log(data1);
+
+
+    const img = document.createElement('img');
+  img.src = 'https://i.pinimg.com/originals/53/e9/45/53e945c516cebdffd987b6c2df159db1.jpg';
+  img.alt = 'AI';
+
+  const profile = document.createElement('div');
+  profile.className = 'profile';
+  profile.appendChild(img);
+
+  const messageContainer = document.createElement('div');
+  messageContainer.className = 'message ai';
+  messageContainer.textContent = "Typing ...";
+
+  const chatBubble = document.createElement('div');
+  chatBubble.className = 'chat';
+  chatBubble.appendChild(profile);
+  chatBubble.appendChild(messageContainer);
+  
+  chatContainer.appendChild(chatBubble);
+
+  // Scroll to bottom of chat container
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // Clear the prompt input
+  e.target.prompt.value = '';
+  
+
+  const prompt = e.target.prompt.value;
+ 
+  const response =await fetch('https://api.openai.com/v1/chat/completions', {
+    
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+ mydata,
+    },
+    body: JSON.stringify({
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": `${prompt}`}]
+    }),
+   
+  });
+  const data =await response.json();
+  const message = data.choices[0].message.content;
+
+  messageContainer.textContent = message;
 
   }
   
@@ -62,57 +109,8 @@ const chatContainer = document.getElementById('chat_container');
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
+  main(e);  
 
-  main();  
-
- 
-  const prompt = e.target.prompt.value;
- 
-  const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+mydata,
-    },
-    body: JSON.stringify({
-      "model": "gpt-3.5-turbo",
-      "messages": [{"role": "user", "content": `${prompt}`}]
-    }),
-   
-  });
-
-  const data = await response.json();
- 
-  const message = data.choices[0].message.content;
-  const chatBubble = document.createElement('div');
-  chatBubble.className = 'chat';
-
-  const profile = document.createElement('div');
-  profile.className = 'profile';
-
-  const img = document.createElement('img');
-  img.src = 'assets/ai.png';
-  img.alt = 'AI';
-
-  profile.appendChild(img);
-
-  const messageContainer = document.createElement('div');
-  messageContainer.className = 'message ai';
-  messageContainer.textContent = message;
-
-  chatBubble.appendChild(profile);
-  chatBubble.appendChild(messageContainer);
-
-  chatContainer.appendChild(chatBubble);
-
-  // Scroll to bottom of chat container
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-
-  // Clear the prompt input
-  e.target.prompt.value = '';
-
-
- 
+  
 });
 
